d1bf3aef8bfda736c94f239ab574957aa75121c8
wendy constantine <wendy@feltpixel.com>
2016-03-13 22:07:31 -0600

    search file

---
+++ app/scripts/controllers/search.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name microbrewMondayApp.controller:SearchCtrl
+ * @description
+ * # SearchCtrl
+ * Controller of the microbrewMondayApp
+ */
+angular.module('microbrewMondayApp')
+  .controller('SearchCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
